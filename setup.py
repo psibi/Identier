8f67520f6883330577f95b35849148ad87995003
Sibi <psibi2000@gmail.com>
2012-05-13 15:17:45 +0530

    For installation

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from setuptools import find_packages
+
+__author__ = 'Sibi <sibi@psibi.in>'
+__version__ = '0.1.0'  #major.minor.micro
+
+setup(
+    # Basic package information.
+    name='identi',
+    version=__version__,
+    packages=find_packages(),
+
+    # Packaging options.
+    include_package_data=True,
+
+    # Package dependencies.
+    install_requires=['oauth2', 'requests', 'requests-oauth'],
+
+    # Metadata for PyPI.
+    author='Sibi',
+    author_email='sibi@psibi.in',
+    license='GNU GPLv3',
+    url='https://github.com/psibi/Identier',
+    keywords='identi.ca API library',
+    description='An easy to use API library specifically for identi.ca',
+    long_description=open('README.md').read(),
+    classifiers=[
+        'Development Status :: 1-Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU GPLv3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Communications :: Chat',
+        'Topic :: Internet'
+    ]
+)
